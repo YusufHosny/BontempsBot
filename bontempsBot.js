b029183a0b3cab47628ea814f4099b602c555a38
Yusuf Hosny <yusufyy2004@hotmail.com>
2023-02-03 15:40:23 +0100

    fixed token being pushed

--- bontempsBot.js
+++ bontempsBot.js
@@ -1,41 +1,15 @@
-const TOKEN = "MTA3MDY5Njk0NTY2NTk3ODQ5OA.GlP9TV.o6wRBJryEA6CMEoVw8RsT3UwpIjtIGKsNrQaK8";
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const { REST, Routes } = require('discord.js');
-
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-];
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-const { Client, GatewayIntentBits } = require('discord.js');
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
-
-client.login(TOKEN);
+// Log in to Discord with your client's token
+client.login(token);
