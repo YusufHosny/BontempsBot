df2e63a19d87a650058c022365a71f45566999a8
Yusuf Hosny <yusufyy2004@hotmail.com>
2023-02-03 15:08:36 +0100

    added bontempsBot.js with sample code inside

---
+++ bontempsBot.js
@@ -0,0 +1,41 @@
+const TOKEN = "MTA3MDY5Njk0NTY2NTk3ODQ5OA.GgjhHq.Ere1LzS8dmdWSsLRglAe4Xl_9aClK1ZbI7s2b4";
+
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
