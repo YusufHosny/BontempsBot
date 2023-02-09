83c2c2854b6ef2670162387abcf10affe804e6fa
Yusuf Hosny <yusufyy2004@hotmail.com>
2023-02-09 03:58:53 +0100

    added demotion and promotion check

--- events/leagueTracker.js
+++ events/leagueTracker.js
@@ -9,18 +9,67 @@ function sleep(s) {
   }
 
 
+
+
+async function getLeagueInfoPuuid(puuid) {
+
+        // Get summoner id from puuid
+        const summonerResult = await request(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${apiKey}`);
+        const summoner = await summonerResult.body.json();
+
+        return await getLeagueInfoSid(summoner.id);
+}
+
+
+
+async function getLeagueInfoSid(summonerid) {
+    // Get player league info
+    const leagueInfoResult = await request(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerid}?api_key=${apiKey}`);
+    const leagueInfoList = await leagueInfoResult.body.json();
+
+    // If the list of league infos has entries, find the solo duo entry and return it
+    if(leagueInfoList.length) 
+    {
+        const leagueInfo = leagueInfoList.find(leagueInfo => leagueInfo.queueType === "RANKED_SOLO_5x5");
+        if(typeof leagueInfo !== 'undefined') return leagueInfo;
+    }
+
+    return null;
+}
+
+
+
 // Pushes a win/loss embed to a specified channel for a specific match participant
-async function pushLeagueEmbed(participant, channel) {
+async function pushLeagueEmbed(participant, rank, channel) {
+
     // Create and format embed
     const embed = new EmbedBuilder();
 
     embed.setColor(participant.win ? "#10b529" : "#990c0c")
         .setTitle(`${participant.summonerName} ${participant.win ? "Won" : "Lost"}!`)
+        .setThumbnail(`https://static.bigbrain.gg/assets/lol/s12_rank_icons/${rank.replace(/ .*/,'').toLowerCase()}.png`)
         .setDescription(participant.win ? "What a fucking legend." :  "What a fucking Loser.")
         .setImage(`https://static.bigbrain.gg/assets/lol/riot_static/13.1.1/img/champion/${participant.championName}.png`)
-        .setFields(
-            { name: 'K/D/A', value: `${participant.kills}/${participant.deaths}/${participant.assists}` }
+        .setFields([
+            { name: 'K/D/A', value: `${participant.kills}/${participant.deaths}/${participant.assists}`, inline: true },
+            { name: 'Rank:', value: rank, inline: true}
+        ]
         );
+    
+    channel.send({ embeds: [embed] });
+
+}
+
+// Pushes a win/loss embed to a specified channel for a specific match participant
+async function pushDemotionEmbed(participant, leagueInfo, channel) {
+
+    // Create and format embed
+    const embed = new EmbedBuilder();
+
+    embed.setColor(participant.win ? "#10b529" : "#990c0c")
+        .setTitle(`${participant.summonerName} ${participant.win ? "PROMOTED" : "DEMOTED"}!`)
+        .setThumbnail(`https://static.bigbrain.gg/assets/lol/s12_rank_icons/${leagueInfo.tier.toLowerCase()}.png`)
+        .setDescription(participant.win ? `CONGRATS!!! BROTHER PROMOTED TO ${leagueInfo.tier}!` :  `BOO HOO. THIS KID DEMOTED TO ${leagueInfo.tier}.`)
     
     channel.send({ embeds: [embed] });
 
@@ -30,6 +79,15 @@ module.exports = {
 	name: Events.ClientReady,
     once: true,
 	async execute(client) {
+        // Create and fill list of ranks for puuids
+        const rankList = [];
+        
+        for (let i = 0; i < puuids.length; i++) {
+            const leagueInfo = await getLeagueInfoPuuid(puuids[i]);
+            if (leagueInfo !== null) rankList.push(leagueInfo.tier); 
+            else  rankList.push("None"); 
+        }
+
         // Fetch bontempsbot channel
         const channel = await client.channels.fetch("1071137172196958239");
 
@@ -63,15 +121,36 @@ module.exports = {
                     // Get the participant we want to display and push the embed
                     const participant = match.info.participants.find(participant => participant.puuid == puuids[index]);
 
-                    pushLeagueEmbed(participant, channel);
+                    // Get participant league info
+                    const leagueInfo = await getLeagueInfoSid(participant.summonerId);
+                    if(leagueInfo !== null)
+                    {   
+                        // If the rank changed
+                        if(leagueInfo.tier !== rankList[index])
+                        {
+                            pushDemotionEmbed(participant, leagueInfo, channel);
+                        }
+                    }
+
+                    pushLeagueEmbed(participant, leagueInfo === null ? "UNRANKED" : `${leagueInfo.tier} ${leagueInfo.rank}`, channel);
 
                     // Check for a duo
-                    const Duo = match.info.participants.find(participant => puuids.includes(participant.puuid) && participant.puuid != puuids[index]);
+                    const duo = match.info.participants.find(participant => puuids.includes(participant.puuid) && participant.puuid != puuids[index]);
 
                     // If there is a duo push embed
-                    if(Duo !== undefined)
+                    if(typeof duo !== 'undefined')
                     {
-                        pushLeagueEmbed(Duo, channel);
+                        const duoleagueInfo = await getLeagueInfoSid(duo.summonerId);
+                        pushLeagueEmbed(duo, duoleagueInfo === null ? "UNRANKED" : `${duoleagueInfo.tier} ${duoleagueInfo.rank}`, channel);
+
+                        if(duoleagueInfo !== null)
+                        {   
+                            // If the rank changed
+                            if(duoleagueInfo.tier !== rankList[puuids.indexOf(duo.puuid)])
+                            {
+                                pushDemotionEmbed(duo, duoleagueInfo, channel);
+                            }
+                        }
                     }
 
                     // Set time stamp to current time
@@ -83,8 +162,8 @@ module.exports = {
             }
 
             
-            // Wait 1 second and increment index
-            await sleep(1);
+            // Wait 2 seconds and increment index
+            await sleep(2);
             index++;
         }
 	},
