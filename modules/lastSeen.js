6c016aef6b9b37f0f8d9941f119ac75a8f471ae1
Yusuf Hosny <yusufyy2004@hotmail.com>
2023-02-16 21:08:19 +0100

    fixed bug: missing ()

--- modules/lastSeen.js
+++ modules/lastSeen.js
@@ -3,6 +3,6 @@ module.exports = {
     getLastSeenString()
     {
         const d = new Date( Date.now() );
-        return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} at ${d.getHours() % 12}:${d.getMinutes < 10 ? '0' : ''}${d.getMinutes()} ${d.getHours() > 11 ? 'PM' : 'AM'}` ;
+        return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} at ${d.getHours() % 12}:${d.getMinutes() < 10 ? '0' : ''}${d.getMinutes()} ${d.getHours() > 11 ? 'PM' : 'AM'}` ;
     },
 }
